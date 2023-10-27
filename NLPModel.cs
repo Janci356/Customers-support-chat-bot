5b3056ccdecb8b56aa042be63344c4de4d2c91df
Janci356 <jmikulec.80@gmail.com>
2023-10-27 20:20:08 +0200

    Project created

---
+++ NLPModel.cs
@@ -0,0 +1,27 @@
+﻿using Catalyst;
+using Mosaik.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers_support_chat_bot
+{
+    internal class NLPModel
+    {
+        public NLPModel() 
+        {
+            Catalyst.Models.English.Register();
+        }
+
+        public async Task<IDocument> Process(string input) 
+        {
+            Pipeline model = await Pipeline.ForAsync(Language.English);
+            Document document = new Document(input, Language.English);
+
+            IDocument processedDoc = model.ProcessSingle(document);
+            return processedDoc;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Catalyst;
+using Customers_support_chat_bot;
+using System.Reflection.Metadata;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        NLPModel model = new NLPModel();
+        Console.WriteLine("Type something:\n");
+        var userInput = Console.ReadLine();
+        IDocument processedInput = await model.Process(userInput is null ? "" : userInput);
+        Console.Write(processedInput.ToJson());
+    }
+}
