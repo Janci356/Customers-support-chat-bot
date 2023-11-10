922411ea356c2b9f47b70a5e443a3a98b5de14bc
xvalabkova <xvalabkova@stuba.sk>
2023-11-10 17:38:58 +0100

    Fix: Id can't be zero

--- UserDbContext.cs
+++ UserDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Customers_support_chat_bot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Customers_support_chat_bot;
 
@@ -15,26 +16,24 @@ public class UserDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasData(
-            new User { UserId = 0, Login = "Cheese", Password = "111", CreatedAt = DateTime.UtcNow},
+            new User { UserId = 3, Login = "Cheese", Password = "111", CreatedAt = DateTime.UtcNow},
             new User { UserId = 1, Login = "Meat", Password = "111", CreatedAt = DateTime.UtcNow },
             new User { UserId = 2, Login = "Fish", Password = "111", CreatedAt = DateTime.UtcNow }
             );
 
         modelBuilder.Entity<Log>().HasData(
-            new Log { LogId = 0, UserId = 1, CreatedAt = DateTime.UtcNow },
             new Log { LogId = 1, UserId = 1, CreatedAt = DateTime.UtcNow },
             new Log { LogId = 2, UserId = 2, CreatedAt = DateTime.UtcNow },
             new Log { LogId = 3, UserId = 1, CreatedAt = DateTime.UtcNow },
             new Log { LogId = 4, UserId = 1, CreatedAt = DateTime.UtcNow },
-            new Log { LogId = 5, UserId = 0, CreatedAt = DateTime.UtcNow },
-            new Log { LogId = 6, UserId = 0, CreatedAt = DateTime.UtcNow }
+            new Log { LogId = 5, UserId = 3, CreatedAt = DateTime.UtcNow },
+            new Log { LogId = 6, UserId = 3, CreatedAt = DateTime.UtcNow }
            );
 
         modelBuilder.Entity<Chat>().HasData(
-            new Chat { ChatId = 0, UserId = 0, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow },
             new Chat { ChatId = 1, UserId = 1, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow },
             new Chat { ChatId = 2, UserId = 2, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow },
-            new Chat { ChatId = 3, UserId = 0, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow },
+            new Chat { ChatId = 3, UserId = 3, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow },
             new Chat { ChatId = 4, UserId = 2, CreatedAt = DateTime.UtcNow, LastChangedAt = DateTime.UtcNow }
            );
     }
