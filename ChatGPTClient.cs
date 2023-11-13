b8830e702ea2cdc91d540c35d3911d8dc68797bb
Janci356 <jmikulec.80@gmail.com>
2023-11-13 13:53:25 +0100

    ChatGpt model added

---
+++ ChatGPTClient.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers_support_chat_bot
+{
+    internal class ChatGPTClient
+    {
+        private readonly string _apiKey;
+        private readonly RestClient _client;
+
+        public ChatGPTClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            _client = new RestClient("https://api.openai.com/v1/chat/completions");
+        }
+
+        public string SendMessage(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return "Sorry, I didn't receive any input. Please try again!";
+            }
+
+            try
+            {
+                var request = new RestRequest("", Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+                var requestBody = new
+                {
+                    prompt = message,
+                    max_tokens = 100,
+                    n = 1,
+                    stop = (string?)null,
+                    temperature = 0.7,
+                    model = "gpt-3.5-turbo",
+                };
+
+                request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+                string jsonRequest = JsonConvert.SerializeObject(request, Formatting.Indented);
+                File.WriteAllText("C:\\Users\\jmiku\\OneDrive\\Desktop\\ASOS\\requests tmp\\request.json", jsonRequest);
+
+                var response = _client.Execute<RestClient>(request);
+
+                string jsonresponse = JsonConvert.SerializeObject(response, Formatting.Indented);
+                File.WriteAllText("C:\\Users\\jmiku\\OneDrive\\Desktop\\ASOS\\responses tmp\\response.json", jsonresponse);
+
+                var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content ?? string.Empty);
+
+
+                return jsonResponse?.choices[0]?.text?.ToString()?.Trim() ?? string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return "Sorry, there was an error processing your request.";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,12 +5,29 @@ using System.Reflection.Metadata;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        NLPModel model = new NLPModel();
-        Console.WriteLine("Type something:\n");
-        var userInput = Console.ReadLine();
-        IDocument processedInput = await model.Process(userInput is null ? "" : userInput);
-        Console.Write(processedInput.ToJson());
+        string apiKey = "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";
+        var chatGPTClient = new ChatGPTClient(apiKey);
+
+        Console.WriteLine("Welcome to the customers support! Type 'exit' to quit.");
+
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // Set text color to green
+            Console.Write("You: ");
+            Console.ResetColor();
+            string input = Console.ReadLine() ?? string.Empty;
+
+            if (input.ToLower() == "exit")
+                break;
+
+            string response = chatGPTClient.SendMessage(input);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Chatbot: ");
+            Console.ResetColor();
+            Console.WriteLine(response);
+        }
     }
 }
