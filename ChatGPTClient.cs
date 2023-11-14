315cd4a57be9bb1190ef258fb32eb28c455b7a8c
Janci356 <jmikulec.80@gmail.com>
2023-11-14 20:09:37 +0100

    Chat Client modified

--- ChatGPTClient.cs
+++ ChatGPTClient.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.Recognizers.Text.DateTime.English;
+using Newtonsoft.Json;
+using OpenAI_API;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -10,6 +13,7 @@ namespace Customers_support_chat_bot
 {
     internal class ChatGPTClient
     {
+        /*
         private readonly string _apiKey;
         private readonly RestClient _client;
 
@@ -35,6 +39,7 @@ namespace Customers_support_chat_bot
                 var requestBody = new
                 {
                     prompt = message,
+                    // 1 token is approximately 4 characters 
                     max_tokens = 100,
                     n = 1,
                     stop = (string?)null,
@@ -61,7 +66,36 @@ namespace Customers_support_chat_bot
             {
                 Console.WriteLine($"Error: {ex.Message}");
                 return "Sorry, there was an error processing your request.";
+            }*/
+
+
+        // Here add API key
+        private OpenAIAPI _api = new OpenAIAPI(new APIAuthentication("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"));
+        private OpenAI_API.Chat.Conversation _conversation { get;  }
+        private ConversationHistory _conversationHistory { get; }
+        public ChatGPTClient()
+        {
+            this._conversation = this._api.Chat.CreateConversation();
+            this._conversationHistory = new ConversationHistory();
+
+            this._conversation.AppendSystemMessage("You are customers support. Never give general answers. You don't have particullar product or application you would respond for accordingly, therefor make up your answers. If you can't give made up answer say: \"I'm not able to help you with this problem. I will move your answer to administrators.\"");
+        }
+
+        public async Task<String> Ask(String question)
+        {
+            String response = "";
+            try
+            {
+                this._conversation.AppendUserInput(question);
+                this._conversationHistory.AddUserMessage(question);
+                response = await this._conversation.GetResponseFromChatbotAsync();
+                this._conversationHistory.AddBotMessage(response);
+            }catch (Exception ex)
+            {
+                return ex.Message;
             }
+
+            return response;
         }
     }
 }
---
+++ ConversationHistory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers_support_chat_bot
+{
+    internal class ConversationHistory
+    {
+        private List<String>? _userMessages { get; set; }
+        private List<String>? _botMessages { get; set; }
+
+        public ConversationHistory() 
+        { 
+            this._userMessages = new List<String>();
+            this._botMessages = new List<String>();
+        }
+
+        public String? AddUserMessage(String message)
+        {
+            try {
+                if (this._userMessages == null) throw new ArgumentNullException(nameof(this._userMessages));
+                else this._userMessages.Add(message);
+            }
+            catch (Exception ex) 
+            { 
+                return ex.Message;
+            }
+
+            return null;
+        }
+
+        public String? AddBotMessage(String message)
+        {
+            try
+            {
+                if (this._botMessages == null) throw new ArgumentNullException(nameof(this._botMessages));
+                else this._botMessages.Add(message);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return null;
+        }
+    }
+}
