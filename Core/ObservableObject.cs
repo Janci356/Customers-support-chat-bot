8572b9f540f9dc94754162372d597377e96228c9
TomasMinarik <tminarik20@gmail.com>
2023-11-15 18:10:35 +0100

    added wpf ui

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers_support_chat_bot.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Customers_support_chat_bot.Core;
+using Customers_support_chat_bot.MVVM.Model;
+using System;
+using System.Collections.ObjectModel;
+namespace Customers_support_chat_bot.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set 
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                if(Message != "" && Message != null)
+                {
+                    Messages.Add(new MessageModel
+                    {
+                        Username = "Tomas",
+                        ImageSource = "./Icons/user.png",
+                        Message = Message,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true,
+                        FirstMessage = true
+                    });
+
+                    Message = "";
+                }
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Tomas",
+                ImageSource = "./Icons/user.png",
+                Message = "Test message",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true,
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Tomas",
+                    ImageSource = "./Icons/user.png",
+                    Message = "Test message",
+                    Time = DateTime.Now,    
+                    IsNativeOrigin = false,
+                    FirstMessage = true,
+                });
+            }
+        }
+    }
+}
