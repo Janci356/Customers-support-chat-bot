a610aeee2095d8585af7bbf3a946b9a8370ef9c1
TomasMinarik <tminarik20@gmail.com>
2023-11-17 11:13:30 +0100

    Implemented chatting

---
+++ ChatGPTClient.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.Recognizers.Text.DateTime.English;
+using Newtonsoft.Json;
+using OpenAI_API;
+using RestSharp;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers_support_chat_bot
+{
+    internal class ChatGPTClient
+    {
+        /*
+        private readonly string _apiKey;
+        private readonly RestClient _client;
+
+        public ChatGPTClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            _client = new RestClient("https://api.openai.com/v1/chat/completions");
+        }
+
+        public string SendMessage(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return "Sorry, I didn't receive any input. Please try again!";
+            }
+
+            try
+            {
+                var request = new RestRequest("", Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+                var requestBody = new
+                {
+                    prompt = message,
+                    // 1 token is approximately 4 characters 
+                    max_tokens = 100,
+                    n = 1,
+                    stop = (string?)null,
+                    temperature = 0.7,
+                    model = "gpt-3.5-turbo",
+                };
+
+                request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+                string jsonRequest = JsonConvert.SerializeObject(request, Formatting.Indented);
+                //File.WriteAllText("C:\\Users\\jmiku\\OneDrive\\Desktop\\ASOS\\requests tmp\\request.json", jsonRequest);
+
+                var response = _client.Execute<RestClient>(request);
+
+                string jsonresponse = JsonConvert.SerializeObject(response, Formatting.Indented);
+                //File.WriteAllText("C:\\Users\\jmiku\\OneDrive\\Desktop\\ASOS\\responses tmp\\response.json", jsonresponse);
+
+                var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content ?? string.Empty);
+
+
+                return jsonResponse?.choices[0]?.text?.ToString()?.Trim() ?? string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return "Sorry, there was an error processing your request.";
+            }*/
+
+
+        // Here add API key
+        private OpenAIAPI _api = new OpenAIAPI(new APIAuthentication(ConfigurationManager.AppSettings["ChatGPTKey"]));
+        private OpenAI_API.Chat.Conversation _conversation { get;  }
+        private ConversationHistory _conversationHistory { get; }
+        public ChatGPTClient()
+        {
+            this._conversation = this._api.Chat.CreateConversation();
+            this._conversationHistory = new ConversationHistory();
+
+            this._conversation.AppendSystemMessage("You are customers support. Never give general answers. You don't have particullar product or application you would respond for accordingly, therefor make up your answers. If you can't give made up answer say: \"I'm not able to help you with this problem. I will move your answer to administrators.\"");
+        }
+
+        public async Task<String> Ask(String question)
+        {
+            String response = "";
+            try
+            {
+                this._conversation.AppendUserInput(question);
+                this._conversationHistory.AddUserMessage(question);
+                response = await this._conversation.GetResponseFromChatbotAsync();
+                this._conversationHistory.AddBotMessage(response);
+            }catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,12 +5,18 @@ using Customers_support_chat_bot.Models;
 using HtmlAgilityPack;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata;
+using System;
+using System.Linq;
+using Microsoft.ML;
 
 class Program
 {
     // Function to create a new user with the given parameters and save to the database
-    private static int CreateUser(DbContext dbContext, string login, string password)
+    public static int CreateUser(DbContext dbContext, string login, string password)
     {
+        // if login is already taken
+        if (GetUser(dbContext, login) != null) return -1;
+
         // Create a new user
         User newUser = new User
         {
@@ -22,68 +28,81 @@ class Program
         return newUser.SaveUser(dbContext);
     }
 
-    // Save the log to the database and user, If User isn't in db return null,
-    // if can't save log into db return -1 
+    // Save the log to the database and user, If User isn't in db or can't save log into db return -1 
     // return LogId if success
-    private static int? CreateLog(DbContext dbContext,int userId, string logPath)
+    public static int CreateLog(DbContext dbContext,int userId, string logPath)
     {
+        var user = User.FindById(dbContext, userId);
+
+        if (user == null)
+        {
+            //Console.WriteLine($"User with ID {userId} not found.");
+            return -1; // or throw an exception, depending on your error handling strategy
+        }
+
         // Create a new log
         Log newLog = new Log
         {
             LogPath = logPath
         };
-        return User.FindById(dbContext, userId)?.AddLogToUserAndDB(dbContext, newLog);        
+        return user.AddLogToUserAndDB(dbContext, newLog);        
     }
 
 
-    // Save the chat to the database and user, If User isn't in db return null,
-    // if can't save chat into db return -1 
+    // Save the chat to the database and user, If User isn't in db  or can't save chat into db return -1 
     // return ChatId if success
-    private static int? CreateChat(DbContext dbContext, int userId, string chatPath)
+    public static int CreateChat(DbContext dbContext, int userId, string chatPath)
     {
+        var user = User.FindById(dbContext, userId);
+
+        if (user == null)
+        {
+            //Console.WriteLine($"User with ID {userId} not found.");
+            return -1; // or throw an exception, depending on your error handling strategy
+        }
         Chat newChat = new Chat
         {
             ChatPath = chatPath
         };
 
-        return User.FindById(dbContext, userId)?.AddChatToUserAndDB(dbContext, newChat);
+        return user.AddChatToUserAndDB(dbContext, newChat);
     }
 
     // Get All logs from db for given userid, If User isn't in db return null,
     // If user hasn't got logs, return empty (i think)
-    private static IEnumerable<Log>? GetUserLogs(DbContext dbContext, int userId)
+    public static IEnumerable<Log>? GetUserLogs(DbContext dbContext, int userId)
     {
         return User.FindById(dbContext, userId)?.GetLogs();
     }
 
     // Get All chats from db for given userid, If User isn't in db return null,
     // If user hasn't got chats return empty (i think)
-    private static IEnumerable<Chat>? GetUserChats(DbContext dbContext, int userId)
+    public static IEnumerable<Chat>? GetUserChats(DbContext dbContext, int userId)
     {
         return User.FindById(dbContext, userId)?.GetChats();
     }
 
     // Get user by id
-    private static User? GetUser(DbContext dbContext, int userId)
+    public static User? GetUser(DbContext dbContext, int userId)
     {
         return User.FindById(dbContext, userId);
     }
     // Get user by login
-    private static User? GetUser(DbContext dbContext, string userLogin)
+    public static User? GetUser(DbContext dbContext, string userLogin)
     {
         return User.FindByLogin(dbContext, userLogin);
     }
-    private static Log? GetLog(DbContext dbContext, int logId)
+    public static Log? GetLog(DbContext dbContext, int logId)
     {
         return Log.FindById(dbContext, logId);
     }
-    private static Chat? GetChat(DbContext dbContext, int chatId)
+    public static Chat? GetChat(DbContext dbContext, int chatId)
     {
         return Chat.FindById(dbContext, chatId);
     }
 
     // Update parameter LastChangedAt to UtcNow, returns true or false if chat no exist
-    private static bool UpdateChat(DbContext dbContext, int chatId)
+    public static bool UpdateChat(DbContext dbContext, int chatId)
     {
         var chat = Chat.FindById(dbContext, chatId);
 
@@ -99,7 +118,7 @@ class Program
     }
 
     // Remove Log from db and user's collection, returns true or false
-    private static bool RemoveLog(DbContext dbContext, int logId)
+    public static bool RemoveLog(DbContext dbContext, int logId)
     {
         var log = Log.FindById(dbContext, logId);
 
@@ -115,7 +134,7 @@ class Program
     }
 
     // Remove Chat from db and user's collection, returns true or false
-    private static bool RemoveChat(DbContext dbContext, int chatId)
+    public static bool RemoveChat(DbContext dbContext, int chatId)
     {
         var chat = Chat.FindById(dbContext, chatId);
 
@@ -128,16 +147,5 @@ class Program
         {
             return false; // User or Chat not found or couldn't be removed
         }
-    }
-
-
-
-    static async Task Main(string[] args)
-    {
-        NLPModel model = new NLPModel();
-        Console.WriteLine("Type something:\n");
-        var userInput = Console.ReadLine();
-        IDocument processedInput = await model.Process(userInput is null ? "" : userInput);
-        Console.Write(processedInput.ToJson());
     }
 }
